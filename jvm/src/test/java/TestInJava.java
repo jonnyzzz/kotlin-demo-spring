f6fbdaf47d1b84d975080af80b56620cca6da707
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-06-04 17:55:26 +0200

    test in java

---
+++ jvm/src/test/java/TestInJava.java
@@ -0,0 +1,13 @@
+import org.jetbrains.demo.JvmKt;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestInJava {
+
+  @Test
+  public void testMessage() {
+//    Assert.assertEquals("", JvmKt.getHi("JUnit"));
+  }
+
+}
+
