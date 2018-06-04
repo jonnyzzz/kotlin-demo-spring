a8ea9834f9162b9d94cb2a66160bcb3c6173a531
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-06-04 16:24:15 +0200

    move

--- server-boot/src/main/java/org/jetbrains/demo/DemoApplication.kt
+++ server-boot/src/main/java/org/jetbrains/demo/DemoApplication.kt
@@ -2,9 +2,9 @@ package org.jetbrains.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
